fd302cc2d64f28c3c48ac6b6788e11fa337b970d
Jake Kendrick <jake.kendrick@gmail.com>
2019-05-17 19:30:17 -0500

    Saving

--- customJS.js
+++ customJS.js
@@ -5,6 +5,22 @@ $(document).ready(function () {
 
 function startupAnimation() {
     $('body').animate({ opacity: "1" }, { duration: 500, queue: false });
-    $('.ani').animate({marginTop: "20px"}, {duration: 500});
+    $('.ani').animate({ marginTop: "20px" }, { duration: 500 });
 }
 
+$(window).resize(function () {
+    let width = $(window).width();
+    if (width <= 768) {
+        $('#main').removeClass('container').addClass('container-fluid');
+    } else {
+        $('#main').removeClass('container-fluid').addClass('container');
+    }
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+$('.popover-dismiss').popover({
+    trigger: 'focus'
+})
